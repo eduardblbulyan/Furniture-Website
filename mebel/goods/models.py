b02f0b477f2f1e3cbc203ed5fd7b61f65c867b73
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-08 21:47:30 +0400

    Customizing displaying in admin panel(names)

--- mebel/goods/models.py
+++ mebel/goods/models.py
@@ -4,6 +4,9 @@ class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name="Название")
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         db_table = "category"
         verbose_name = "Категорию" 
@@ -18,6 +21,9 @@ class Product(models.Model):
     discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2,verbose_name="Скидка в %")
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
     category = models.ForeignKey(Categories,on_delete=models.CASCADE ,verbose_name='Категория')
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         db_table = "product"
