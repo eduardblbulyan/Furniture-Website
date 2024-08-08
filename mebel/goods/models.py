6d1068f1d066f61c25302293bcace04a9f792abe
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-08 21:25:14 +0400

    Adding product to admin

--- mebel/goods/models.py
+++ mebel/goods/models.py
@@ -8,3 +8,18 @@ class Categories(models.Model):
         db_table = "category"
         verbose_name = "Категорию" 
         verbose_name_plural = "Категории" 
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="Название")
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
+    description = models.TextField(verbose_name="Описание", blank=True, null=True)
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2,verbose_name="Цена")
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2,verbose_name="Скидка в %")
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    category = models.ForeignKey(Categories,on_delete=models.CASCADE ,verbose_name='Категория')
+
+    class Meta:
+        db_table = "product"
+        verbose_name = "Продукт" 
+        verbose_name_plural = "Продукты"
