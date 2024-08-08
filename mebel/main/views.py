67be2489b166d889545224fcd20fc6bcae1347b2
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-08 11:44:35 +0400

    Adding simple template

--- mebel/main/views.py
+++ mebel/main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Home page")
+    return render(request, 'main/index.html')
 
 def about(request):
     return HttpResponse("About page")
