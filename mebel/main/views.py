7f31779ac1c42881ec8fc46f99643c6dc837e54d
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-08 12:07:44 +0400

    Adding main template of index page

--- mebel/main/views.py
+++ mebel/main/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    context = {
+        "title": "Home",
+        "content": "Some content about furnitures"
+    }
+    return render(request, 'main/index.html', context)
 
 def about(request):
     return HttpResponse("About page")
