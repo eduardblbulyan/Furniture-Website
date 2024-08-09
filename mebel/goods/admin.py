0d34371c472a02001720dfe00cf0baba4af30305
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-09 17:36:28 +0400

    Menu and goods from DB

--- mebel/goods/admin.py
+++ mebel/goods/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Categories, Product
 
-admin.site.register([Categories,Product])
+# admin.site.register([Categories,Product])
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
     
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
--- mebel/main/views.py
+++ mebel/main/views.py
@@ -1,18 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from goods.models import Categories
 
 
 def index(request):
+    categories = Categories.objects.all()
     context = {
         "title": "Home - Главная",
-        "content": "Магазин мебели HOME"
+        "content": "Магазин мебели HOME",
+        "categories": categories
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
+    categories = Categories.objects.all()
     context = {
         "title": "Home - О нас",
         "content": "Информация о магазине",
-        "text_on_page": "Какой то текст!!" # avoid hyphens (-) because template can not understand text-on-page
+        "text_on_page": "Какой то текст!!", # avoid hyphens (-) because template can not understand text-on-page
+        "categories": categories
     }
     return render(request, 'main/about.html', context)
