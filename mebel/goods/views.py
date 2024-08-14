ff0c6a3403c1feeb7cccb86fc8bb8aa3cafa000f
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-14 19:13:06 +0400

    Switch prod urls to slug

--- mebel/goods/views.py
+++ mebel/goods/views.py
@@ -9,7 +9,7 @@ def catalog(request):
     }
     return render(request, "goods/catalog.html", context)
 
-def product(request, product_id):
-    product = Product.objects.get(id=product_id)
+def product(request, product_slug):
+    product = Product.objects.get(slug=product_slug)
     return render(request, "goods/product.html", {"product": product})
 
