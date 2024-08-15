b022d2eda43645c675c108d7cb64876526b2db94
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-15 19:28:15 +0400

    Set up buttons

--- mebel/goods/views.py
+++ mebel/goods/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_list_or_404
 from django.core.paginator import Paginator
 from .models import Categories, Product
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
     if category_slug=="all":
         goods = Product.objects.all()
     else:
@@ -10,11 +10,12 @@ def catalog(request, category_slug):
         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
 
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(1)
+    current_page = paginator.page(page)
         
     context = { # temporary mock db emulation
         'title': 'Home - Catalog',
-        "goods": current_page
+        "goods": current_page,
+        "slug_url": category_slug
     }
     return render(request, "goods/catalog.html", context)
 
